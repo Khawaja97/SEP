d3ef3169c09f29e34712e9647d35a26592f5d23c
Khawaja97 <70969696+Khawaja97@users.noreply.github.com>
2021-06-10 12:06:08 -0400

    Add files via upload

---
+++ Assignment1/Exercise7/ATM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class ATM
+    {
+        private decimal balance;
+
+        public ATM()
+        {
+            this.balance = 1000;
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine("Your current balance is: " + this.balance);
+        }
+
+        public void DepositCash(decimal amount)
+        {
+            this.balance += amount;
+            Console.WriteLine("Deposit successful!!");
+            Console.WriteLine($"Your new balance is {this.balance}");
+            
+        }
+
+        public void WithdrawCash(decimal amount)
+        {
+            if(amount > this.balance)
+            {
+                Console.WriteLine("The maximum amount you can withdraw is " + this.balance); 
+            }
+            else
+            {
+                this.balance -= amount;
+                Console.WriteLine("Withdraw successful!!");
+                Console.WriteLine($"Your new balance is {this.balance}");
+            }
+
+
+        }
+    }
+}
